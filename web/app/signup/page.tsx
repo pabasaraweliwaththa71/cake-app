3dba19f4b3bc3844fd1c0c37795925e7d02cb8fe
pabasaraweliwaththa71 <cst20071@std.uwu.ac.lk>
2024-05-23 09:57:10 +0530

    page

---
+++ web/app/signup/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+export default function Page() {
+  // handle form submission
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    // add role to the form data
+    data.append("role", "user");
+
+    const value = Object.fromEntries(data.entries());
+
+    fetch("http://localhost:5000/v1/api/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(value),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        // redirect to dashboard
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-50">
+      <div className="p-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl font-semibold text-gray-800">Sign up</h1>
+        <form className="mt-4 space-y-4" onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              placeholder="Name"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              placeholder="
+                Email address"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              placeholder="Password"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Sign in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
