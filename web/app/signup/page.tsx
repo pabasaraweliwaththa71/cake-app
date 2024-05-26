806c6fea36ae7a9dff1a60903b9bc6c2d3b9669b
pabasaraweliwaththa71 <cst20071@std.uwu.ac.lk>
2024-05-26 14:34:06 +0530

    login

--- web/app/signup/page.tsx
+++ web/app/signup/page.tsx
@@ -81,12 +81,7 @@ export default function Page() {
             />
           </div>
 
-          <button
-            type="submit"
-            className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Sign in
-          </button>
+         
         </form>
       </div>
     </div>
