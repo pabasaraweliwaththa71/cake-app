3c478590498ba7329f8f0267fa3698f5ba197e63
SalihaNaiser <109573036+SalihaNaiser@users.noreply.github.com>
2024-06-13 13:05:24 +0530

    Update page.tsx

--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -1,9 +1,26 @@
+// Enable client-side rendering
 "use client";
 
+// Exporting the default Page component
 export default function Page() {
-  // handle form submission
+  // Function to handle form submission
   function handleSubmit(event: any) {
-    event.preventDefault();
-    const data = new FormData(event.target);
 
+    // Prevent default form submission
+    event.preventDefault();
+    // Collect form data
+    const data = new FormData(event.target);
+// Convert form data to object
     const value = Object.fromEntries(data.entries());
+
+   // Send form data to server for login 
+    fetch("http://localhost:5000/v1/api/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(value),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
