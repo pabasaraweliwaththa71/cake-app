28119125caa40234af54d5c8ef5149b2357fde04
SalihaNaiser <109573036+SalihaNaiser@users.noreply.github.com>
2024-06-15 03:58:19 +0530

    Update profile\page.tsx

--- web/app/profile/page.tsx
+++ web/app/profile/page.tsx
@@ -21,3 +21,39 @@ export default function Page() {
       province: "",
       phone: "",
     }); // State for storing form data
+
+    // Function to fetch user data from the server
+    const getUserData = async () => {
+        // Get token from localStorage
+        const token = localStorage.getItem("token");
+        if (token) {
+            // Decode the token to get user ID
+          const decodedToken = jwtDecode(token) as any;
+          const response = await fetch(
+            // Fetch user data using the user ID
+            `http://localhost:5000/v1/api/user/${decodedToken.id}`
+          );
+          // Parse the response to JSON
+          const data = await response.json();
+          // Update form data with fetched user data
+          setFormData(data);
+        }
+      };
+
+      useEffect(() => {
+        // Fetch user data when the component mounts
+        getUserData();
+      }, []);
+    
+      // validate password
+      function validateForm() {
+        let errors = {} as any;
+    
+        if (formData.password && formData.password.length < 6) {
+          errors.password = "Password must be at least 6 characters";
+        }
+        if (formData.password !== formData.confirmPassword) {
+          errors.confirmPassword = "Passwords do not match";
+        }
+        return errors;
+      }
