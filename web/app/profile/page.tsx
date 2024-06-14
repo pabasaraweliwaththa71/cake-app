e6b8715d382fecceb7442140e9d1e6ba0d484c47
SalihaNaiser <109573036+SalihaNaiser@users.noreply.github.com>
2024-06-14 09:50:52 +0530

    update

--- web/app/profile/page.tsx
+++ web/app/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+/*import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import Link from "next/link";
 
