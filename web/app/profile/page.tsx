0623bffa52948e2193a0bc9fde2026af9711ef8c
SalihaNaiser <109573036+SalihaNaiser@users.noreply.github.com>
2024-06-14 21:36:33 +0530

    Update profile\page.tsx

--- web/app/profile/page.tsx
+++ web/app/profile/page.tsx
@@ -1,5 +1,23 @@
+// This directive ensures the component is rendered on the client side
 "use client";
 
+// Importing hooks from React
 import { useEffect, useState } from "react";
+// Importing jwt-decode library for decoding JWT tokens
 import { jwtDecode } from "jwt-decode";
+// Importing Link from Next.js for client-side navigation
 import Link from "next/link";
+
+export default function Page() {
+    // State for storing validation errors
+    const [errors, setErrors] = useState({} as any);
+    const [formData, setFormData] = useState({
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      role: "",
+      address: "",
+      province: "",
+      phone: "",
+    }); // State for storing form data
