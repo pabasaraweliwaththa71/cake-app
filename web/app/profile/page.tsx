27b17e4244608bd3b33418bb4d12ba2172952094
SalihaNaiser <109573036+SalihaNaiser@users.noreply.github.com>
2024-06-16 09:34:18 +0530

    update profile\page.tsx

--- web/app/profile/page.tsx
+++ web/app/profile/page.tsx
@@ -150,6 +150,7 @@ export default function Page() {
 
   return (
     <>
+    {/* Navigation links */}
       <div className="flex justify-center items-center space-x-4 p-4">
         <Link
           href="/profile/orders"
@@ -164,12 +165,16 @@ export default function Page() {
           Customized Cake Request
         </Link>
       </div>
+
+      {/* Profile form */}
       <div className="flex items-center justify-center h-screen mt-10  ">
         <div className="p-8 bg-white rounded-lg shadow-md">
           <h1 className="text-2xl font-semibold text-gray-800">
             My Profile ({formData.role})
           </h1>
           <form className="mt-4 space-y-4">
+
+             {/* Name input */}
             <div>
               <label
                 htmlFor="name"
@@ -188,8 +193,28 @@ Name
                 placeholder="Name"
               />
             </div>
+
+            {/* Email input */}
             <div>
               <label
                 htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+
+Email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-orange-400 focus:border-orange-400 sm:text-sm"
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="address"
                 className="block text-sm font-medium text-gray-700"
               >
