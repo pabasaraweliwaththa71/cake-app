a37ed5d365036f0885dc7e055f15506c8e3ac1c9
SalihaNaiser <109573036+SalihaNaiser@users.noreply.github.com>
2024-06-17 13:53:15 +0530

    Update user.controller.js

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,13 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
+
+// Function to create a new user
+exports.createUser = async (req, res) => {
+    try {
+
+    } catch (error) {
+        // Send an error response if user creation fails
+        res.status(500).json({ error: "Failed to create user" });
+      }
+    };
