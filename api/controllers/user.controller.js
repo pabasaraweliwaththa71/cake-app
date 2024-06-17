eda348caacdeedd7be4d3b5b7569c209f81b6127
SalihaNaiser <109573036+SalihaNaiser@users.noreply.github.com>
2024-06-17 15:10:09 +0530

    update user.controller.js

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -6,6 +6,38 @@ var jwt = require("jsonwebtoken");
 exports.createUser = async (req, res) => {
     try {
 
+        const { name, email, address, phone, password, role, province } = req.body;
+
+        // Check if the user already exists
+        const existing = await User.findOne({ email });
+        if (existing) {
+          return res.status(400).json({ error: "User already exists" });
+        }
+    
+        // hash the password before saving
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+    
+        // Create a new user instance
+        const user = new User({
+          name,
+          email,
+          role,
+          address,
+          phone,
+          province,
+          password: hashedPassword,
+        });
+
+        // Save the user to the database
+        const savedPost = await user.save();
+
+        // Send a success response
+    res.status(201).json({
+      message: "successful",
+      user: savedPost,
+    });
+ 
     } catch (error) {
         // Send an error response if user creation fails
         res.status(500).json({ error: "Failed to create user" });
