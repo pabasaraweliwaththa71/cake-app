5401f6f3f9b5e9c49b23c3cace8498c2ef97ba30
SalihaNaiser <109573036+SalihaNaiser@users.noreply.github.com>
2024-06-19 04:50:19 +0530

    Update page.tsx

---
+++ web/app/admin/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+// Default export of the Page component, which represents the main content of the Admin Panel Dashboard.
+export default function Page() {
+  return (
+    <div className="text-2xl font-semibold h-screen flex items-center justify-center">
+      Admin Panel Dashboard
+    </div>
+  );
+}
