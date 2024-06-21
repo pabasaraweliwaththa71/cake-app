612afdc94f56c934c160e4580577a1aca7739b22
SalihaNaiser <109573036+SalihaNaiser@users.noreply.github.com>
2024-06-21 10:20:04 +0530

    update page.tsx

--- web/app/profile/customized-cake-request/page.tsx
+++ web/app/profile/customized-cake-request/page.tsx
@@ -75,92 +75,121 @@ export default function UserCustomCakeRequests() {
           <h1 className="text-2xl font-semibold text-gray-800 mb-8">
             My Customized Cake Requests
           </h1>
-
           <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {requests.map((request: any) => (
-          <div key={request._id} className="p-6 bg-white rounded-lg shadow-md">
-            <img
-              className="w-full h-48 object-cover rounded-md mb-4"
-              src={request.image}
-              alt={request.flavor}
-            />
-            <h2 className="text-xl font-semibold text-gray-800">
-              {request.flavor}
-            </h2>
-            <div className="border-t border-gray-200 mt-4"></div>
-            <p className="text-gray-700 mt-2">Topping: {request.topping}</p>
-            <p className="text-gray-700 mt-2">Topper: {request.topper}</p>
-            <p className="text-gray-700 mt-2">
-              Decoration: {request.decoration}
-            </p>
-            <p className="text-gray-700 mt-2">Weight: {request.weight} kg</p>
-            <p className="text-gray-700 mt-2">Message: {request.message}</p>
-            <p className="text-gray-700 mt-2">
-              Extra Details: {request.extraDetails}
-            </p>
-
-            <div className="flex space-x-4 mt-4">
-              <div className="flex items-center">
-                <input
-                  id="glutenFree"
-                  name="glutenFree"
-                  type="checkbox"
-                  checked={request.glutenFree}
-                  className="h-4 w-4 text-orange-700 border-gray-300 rounded focus:ring-orange-400"
+            {requests.map((request: any) => (
+              <div key={request._id} className="p-6 bg-white rounded-lg shadow-md">
+                <img
+                  className="w-full h-48 object-cover rounded-md mb-4"
+                  src={request.image}
+                  alt={request.flavor}
                 />
-                <label
-                  htmlFor="glutenFree"
-                  className="ml-2 block text-sm text-gray-900"
-                >
-                  Gluten Free
-                </label>
+                <h2 className="text-xl font-semibold text-gray-800">
+                  {request.flavor}
+                </h2>
+                <div className="border-t border-gray-200 mt-4"></div>
+                <p className="text-gray-700 mt-2">Topping: {request.topping}</p>
+                <p className="text-gray-700 mt-2">Topper: {request.topper}</p>
+                <p className="text-gray-700 mt-2">
+                  Decoration: {request.decoration}
+                </p>
+                <p className="text-gray-700 mt-2">Weight: {request.weight} kg</p>
+                <p className="text-gray-700 mt-2">Message: {request.message}</p>
+                <p className="text-gray-700 mt-2">
+                  Extra Details: {request.extraDetails}
+                </p>
+    
+                <div className="flex space-x-4 mt-4">
+                  <div className="flex items-center">
+                    <input
+                      id="glutenFree"
+                      name="glutenFree"
+                      type="checkbox"
+                      checked={request.glutenFree}
+                      className="h-4 w-4 text-orange-700 border-gray-300 rounded focus:ring-orange-400"
+                    />
+                    <label
+                      htmlFor="glutenFree"
+                      className="ml-2 block text-sm text-gray-900"
+                    >
+                      Gluten Free
+                    </label>
+                  </div>
+                  <div className="flex items-center">
+                    <input
+                      id="vegan"
+                      name="vegan"
+                      type="checkbox"
+                      checked={request.vegan}
+                      className="h-4 w-4 text-orange-700 border-gray-300 rounded focus:ring-orange-400"
+                    />
+                    <label
+                      htmlFor="vegan"
+                      className="ml-2 block text-sm text-gray-900"
+                    >
+                      Vegan
+                    </label>
+                  </div>
+                  <div className="flex items-center">
+                    <input
+                      id="nutFree"
+                      name="nutFree"
+                      type="checkbox"
+                      checked={request.nutFree}
+                      className="h-4 w-4 text-orange-700 border-gray-300 rounded focus:ring-orange-400"
+                    />
+                    <label
+                      htmlFor="nutFree"
+                      className="ml-2 block text-sm text-gray-900"
+                    >
+                      Nut Free
+                    </label>
+                  </div>
+                </div>
+    
+                <div className="border-t border-gray-200 mt-4"></div>
+                <p className="text-gray-700 mt-2">
+                  Delivery Date: {new Date(request.deliveryDate).toDateString()}
+                </p>
+                <p className="text-gray-700 mt-2">Price: LKR {request.price}</p>
+    
+                <div className="border-t border-gray-200 mt-4">
+                  {request.accepted && (
+                    <p className="text-green-600 mt-2">Order Accepted 😄</p>
+                  )}
+                </div>
+                <p className="text-gray-700 mt-2">Status: {request.status}</p>
+    
+                <div className="mt-4 flex space-x-4">
+                  <button
+                    disabled={request.accepted}
+                    onClick={() => {
+                      if (
+                        window.confirm(
+                          "Are you sure you want to cancel this customized cake request?"
+                        )
+                      ) {
+                        handleCancelRequest(request._id);
+                      }
+                    }}
+                    className={`${
+                      request.accepted
+                        ? "bg-gray-200 cursor-not-allowed"
+                        : "bg-red-500 hover:bg-red-600"
+                    } text-white py-2 px-4 rounded-md`}
+                  >
+                    Cancel Request
+                  </button>
+                </div>
+                {request.accepted && (
+                  <p className="mt-2 text-[10px]">
+                    You can't cancel this request because it has been accepted.
+                    Please wait for the delivery.
+                  </p>
+                )}
               </div>
-              <div className="flex items-center">
-                <input
-                  id="vegan"
-                  name="vegan"
-                  type="checkbox"
-                  checked={request.vegan}
-                  className="h-4 w-4 text-orange-700 border-gray-300 rounded focus:ring-orange-400"
-                />
-                <label
-                  htmlFor="vegan"
-                  className="ml-2 block text-sm text-gray-900"
-                >
-                  Vegan
-                </label>
-              </div>
-              <div className="flex items-center">
-                <input
-                  id="nutFree"
-                  name="nutFree"
-                  type="checkbox"
-                  checked={request.nutFree}
-                  className="h-4 w-4 text-orange-700 border-gray-300 rounded focus:ring-orange-400"
-                />
-                <label
-                  htmlFor="nutFree"
-                  className="ml-2 block text-sm text-gray-900"
-                >
-                  Nut Free
-                </label>
-              </div>
-            </div>
-
-            <div className="border-t border-gray-200 mt-4"></div>
-            <p className="text-gray-700 mt-2">
-              Delivery Date: {new Date(request.deliveryDate).toDateString()}
-            </p>
-            <p className="text-gray-700 mt-2">Price: LKR {request.price}</p>
-
-            <div className="border-t border-gray-200 mt-4">
-              {request.accepted && (
-                <p className="text-green-600 mt-2">Order Accepted 😄</p>
-              )}
-            </div>
-            <p className="text-gray-700 mt-2">Status: {request.status}</p>
-
-            </div>
+            ))}
           </div>
-  );
-}
+        </div>
+      );
+    }
+    
