8f24c55598692c79cf7878c3861e736acb433eb7
SalihaNaiser <109573036+SalihaNaiser@users.noreply.github.com>
2024-06-22 14:13:34 +0530

    update page.tsx

--- web/app/profile/orders/page.tsx
+++ web/app/profile/orders/page.tsx
@@ -3,14 +3,20 @@
 import { useEffect, useState } from "react";
 import useUserData from "@/hook/useUser";
 
+// Component for displaying user's orders
 export default function UserOrdersPage() {
+    // Fetch user data using custom hook
     const user = useUserData();
+    // State to store the user's orders
     const [orders, setOrders] = useState([]) as any;
   
+    // Function to fetch user's orders from the server
     const fetchUserOrders = async () => {
+        // Check if user data is available
       if (user) {
         fetch(`http://localhost:5000/v1/api/orders/user/${user.id}`)
           .then((response) => response.json())
+          // Update state with fetched data
           .then((data) => setOrders(data))
           .catch((error) => console.error("Error fetching user orders:", error));
       }
