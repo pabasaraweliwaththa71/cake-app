c007deae3f749fcafd02ab79533b4bd3b91ea8ee
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2024-06-26 18:01:09 +0530

    create workshop

--- api/controllers/workshop.controller.js
+++ api/controllers/workshop.controller.js
@@ -1,1 +1,23 @@
 const { Workshop, Registration } = require("../models/workshop.model");
+exports.createWorkshop = async (req, res) => {
+  try {
+    const { title, description, date, link, price, instructor, image } =
+      req.body;
+
+    const workshop = new Workshop({
+      title,
+      description,
+      date,
+      link,
+      price,
+      instructor,
+      image,
+    });
+    const savedWorkshop = await workshop.save();
+
+    res.status(201).json(savedWorkshop);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to create workshop" });
+  }
+};
