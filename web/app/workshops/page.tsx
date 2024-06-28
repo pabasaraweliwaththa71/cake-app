6fe19e1d5f41ed64ff78f0db57eb1625fe15b299
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2024-06-28 18:59:09 +0530

    workshop list

--- web/app/workshops/page.tsx
+++ web/app/workshops/page.tsx
@@ -3,5 +3,13 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
+const WorkshopList = () => {
+  const [workshops, setWorkshops] = useState([]) as any;
 
+  useEffect(() => {
+    fetch("http://localhost:5000/v1/api/workshops")
+      .then((response) => response.json())
+      .then((data) => setWorkshops(data))
+      .catch((error) => console.error("Error fetching workshops:", error));
+  }, []);
 export default WorkshopList;
