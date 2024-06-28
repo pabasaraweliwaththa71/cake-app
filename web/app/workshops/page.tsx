13f153556ffcdb3925a14c520f0f010b07c8e012
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2024-06-28 19:00:16 +0530

    upcomming workshops

--- web/app/workshops/page.tsx
+++ web/app/workshops/page.tsx
@@ -12,4 +12,17 @@ const WorkshopList = () => {
       .then((data) => setWorkshops(data))
       .catch((error) => console.error("Error fetching workshops:", error));
   }, []);
+  return (
+    <div className="flex flex-col items-center justify-center   py-8">
+      <h1 className="text-2xl font-semibold text-gray-800 mb-8">
+        Upcoming Workshops
+      </h1>
+      <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {workshops.map((workshop: any) => (
+          <div key={workshop._id} className="p-6 bg-white rounded-lg shadow-md">
+            <img
+              className="w-full h-48 object-cover rounded-md mb-4"
+              src={workshop.image}
+              alt={workshop.title}
+            />
 export default WorkshopList;
