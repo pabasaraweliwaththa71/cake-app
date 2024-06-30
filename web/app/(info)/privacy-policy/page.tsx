97ce370a6c74c0b586ed05ee046f794a54ea4cbc
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2024-06-30 19:21:26 +0530

    description

--- web/app/(info)/privacy-policy/page.tsx
+++ web/app/(info)/privacy-policy/page.tsx
@@ -2,5 +2,13 @@
 
 export default function PrivacyPolicy() {
   
-  
+  <div className="container mx-auto p-8">
+      <h1 className="text-4xl font-bold mb-4">Privacy Policy</h1>
+      <p className="text-lg mb-4">
+        At Our Cake Shop, we value your privacy and are committed to protecting
+        your personal information. This Privacy Policy outlines how we collect,
+        use, and safeguard your data when you interact with our website and
+        services.
+      </p>
+</div>
 }
