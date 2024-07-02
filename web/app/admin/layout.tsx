4e176e8ebf1f1e8b4458f11bfe1828f92cb3fd81
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2024-07-02 18:19:49 +0530

    Admin panel

--- web/app/admin/layout.tsx
+++ web/app/admin/layout.tsx
@@ -4,4 +4,11 @@ import useUserData from "@/hook/useUser";
 import Link from "next/link";
 
 function Sidebar() {
+   return (
+    <div className="w-64 h-screen bg-stone-700 text-white p-6 rounded-md">
+      <h2 className="flex justify-center text-center text-sm font-bold mb-6 text-stone-300">
+        Admin Panel
+      </h2>
+      </div>
+   )
 }
