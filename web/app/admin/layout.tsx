c3f72309d1846232fc46d653e5e918e9beb75889
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2024-07-02 18:21:44 +0530

    admi handle customized cake and add cake

--- web/app/admin/layout.tsx
+++ web/app/admin/layout.tsx
@@ -5,19 +5,32 @@ import Link from "next/link";
 
 function Sidebar() {
    return (
-    <div className="w-64 h-screen bg-stone-700 text-white p-6 rounded-md">
-      <h2 className="flex justify-center text-center text-sm font-bold mb-6 text-stone-300">
-        Admin Panel
-      </h2>
-      <nav className="flex flex-col space-y-4">
-        <p className="text-lg font-semibold !mt-10">Manage Orders</p>
-        <Link
-          className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded text-sm"
-          href="/admin/orders"
-        >
-          All Orders
-        </Link>
-        </nav>
-      </div>
-   )
+     <div className="w-64 h-screen bg-stone-700 text-white p-6 rounded-md">
+       <h2 className="flex justify-center text-center text-sm font-bold mb-6 text-stone-300">
+         Admin Panel
+       </h2>
+       <nav className="flex flex-col space-y-4">
+         <p className="text-lg font-semibold !mt-10">Manage Orders</p>
+         <Link
+           className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded text-sm"
+           href="/admin/orders"
+         >
+           All Orders
+         </Link>
+         <Link
+           className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded text-sm"
+           href="/admin/customized-cakes"
+         >
+           Customized Cake Orders
+         </Link>
+         <p className="text-lg font-semibold !mt-10">Manage Inventory</p>
+         <Link
+           className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded text-sm"
+           href="/admin/cake"
+         >
+           Add Cake
+         </Link>
+       </nav>
+     </div>
+   );
 }
