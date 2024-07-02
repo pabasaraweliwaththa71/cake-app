e79e5c83b80dc7e0b3eebfb44422c93fb48dbfb4
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2024-07-02 18:22:30 +0530

    admin add worshops

--- web/app/admin/layout.tsx
+++ web/app/admin/layout.tsx
@@ -30,6 +30,12 @@ function Sidebar() {
          >
            Add Cake
          </Link>
+         <Link
+           className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded text-sm"
+           href="/admin/workshops"
+         >
+           Add Workshops
+         </Link>
        </nav>
      </div>
    );
