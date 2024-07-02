18b1377868e15a4eff14a8a280fe2d8bb2a6c1af
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2024-07-02 18:23:05 +0530

    admin add tutorials

--- web/app/admin/layout.tsx
+++ web/app/admin/layout.tsx
@@ -36,6 +36,12 @@ function Sidebar() {
          >
            Add Workshops
          </Link>
+         <Link
+           className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded text-sm"
+           href="/admin/tutorials"
+         >
+           Add Tutorials
+         </Link>
        </nav>
      </div>
    );
