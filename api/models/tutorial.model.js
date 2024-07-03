f4ba0f7154b5d157abdf09cbd054a70eb25b12be
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2024-07-03 17:24:31 +0530

    tutorial models

--- api/models/tutorial.model.js
+++ api/models/tutorial.model.js
@@ -1,24 +1,2 @@
 const mongoose = require("mongoose");
 
-const tutorialchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  published: {
-    type: Boolean,
-    default: true,
-  },
-  url: {
-    type: String,
-    required: true,
-  },
-});
-
-const Tutorial = mongoose.model("Tutorial", tutorialchema);
-
-module.exports = Tutorial;
