bd1bb812a8332d19cd915914f2070c2c6cbaf2f1
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2024-07-03 17:25:26 +0530

    update

--- api/models/tutorial.model.js
+++ api/models/tutorial.model.js
@@ -1,2 +1,20 @@
 const mongoose = require("mongoose");
 
+const tutorialchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  published: {
+    type: Boolean,
+    default: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+});
