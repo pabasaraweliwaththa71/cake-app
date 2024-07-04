23f17737f8db309aff6cbf6556182b2e7a1174ad
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2024-07-04 18:49:25 +0530

    workshop schema

--- api/models/workshop.model.js
+++ api/models/workshop.model.js
@@ -1,2 +1,11 @@
 const mongoose = require("mongoose");
 
+const workshopSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, required: true },
+  link: { type: String, required: true },
+  price: { type: Number, required: true },
+  instructor: { type: String, required: true },
+  image: { type: String },
+});
