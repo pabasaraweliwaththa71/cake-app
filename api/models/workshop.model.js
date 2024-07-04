5942940b62ff1af027fe2ee61086c37b17fc5594
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2024-07-04 18:50:03 +0530

    registration schema

--- api/models/workshop.model.js
+++ api/models/workshop.model.js
@@ -9,3 +9,14 @@ const workshopSchema = new mongoose.Schema({
   instructor: { type: String, required: true },
   image: { type: String },
 });
+const registrationSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  workshop: { type: mongoose.Schema.Types.ObjectId, ref: "Workshop" },
+  attendees: { type: Number, required: true },
+  paymentStatus: { type: String, default: "pending" },
+});
+
+const Workshop = mongoose.model("Workshop", workshopSchema);
+const Registration = mongoose.model("WorkshopRegistration", registrationSchema);
+
+module.exports = { Workshop, Registration };
